dfe2702e72a88d3e0c08a3b4cd5f914e3dcee143
Ryan Stelly <ryan.stelly@live.com>
2025-09-27 22:40:01 -0500

    use storybook lostpixel config

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,10 +1,8 @@
 import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
-  pageShots: {
-    pages: [{ path: "/", name: "landing" }],
-    baseUrl: "http://172.17.0.1:3000",
-  },
+  storybookShots: { storybookUrl: "storybook-static" },
+
   lostPixelProjectId: "cmg33d82906m011p1m06dj8xt",
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
